b6adabd9f2bef804f3e3917d427c05eb281b953a
Akshara Nigam <akshara.nigam@hotstar.com>
2021-11-24 12:59:01 +0530

    Adds msg transformation

--- main.go
+++ main.go
@@ -8,10 +8,9 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 )
 
-
 func main() {
 	sess, err := session.NewSession(&aws.Config{
-		Region:      aws.String("us-west-2"),
+		Region:      aws.String("us-east-1"),
 		Credentials: credentials.NewSharedCredentials("", "devmfa"),
 	})
 
@@ -20,12 +19,15 @@ func main() {
 	}
 
 	queueName := "Test-Queue-Akshara"
-
+	msgMap := make(map[string]string)
+	msgMap["Author"] = "John Grisham"
+	msgMap["Title"] = "The Whistler"
+	msgMap["WeeksOn"] = "6"
 
 	ListQueues(sess)
 	CreateQueue(sess, &queueName)
 	url := GetQueueURL(sess, &queueName)
-	SendMessage(sess, url)
+	SendMessage(sess, url, msgMap)
 	ReceiveMessage(sess, url)
 	DeleteQueue(sess, &queueName)
 	ListQueues(sess)
--- send-message.go
+++ send-message.go
@@ -8,27 +8,15 @@ import (
 	"github.com/aws/aws-sdk-go/service/sqs"
 )
 
-func SendMessage(sess *session.Session, queueURL *string) {
+func SendMessage(sess *session.Session, queueURL *string, msgMap map[string]string) {
 	svc := sqs.New(sess)
+	msg := transformToSQSMap(msgMap)
 
 	_, err := svc.SendMessage(&sqs.SendMessageInput{
-		DelaySeconds: aws.Int64(10),
-		MessageAttributes: map[string]*sqs.MessageAttributeValue{
-			"Title": &sqs.MessageAttributeValue{
-				DataType:    aws.String("String"),
-				StringValue: aws.String("The Whistler"),
-			},
-			"Author": &sqs.MessageAttributeValue{
-				DataType:    aws.String("String"),
-				StringValue: aws.String("John Grisham"),
-			},
-			"WeeksOn": &sqs.MessageAttributeValue{
-				DataType:    aws.String("Number"),
-				StringValue: aws.String("6"),
-			},
-		},
-		MessageBody: aws.String("Information about current NY Times fiction bestseller for week of 12/11/2016."),
-		QueueUrl:    queueURL,
+		DelaySeconds:      aws.Int64(10),
+		MessageAttributes: msg,
+		MessageBody:       aws.String("Information about current NY Times fiction bestseller for week of 12/11/2016."),
+		QueueUrl:          queueURL,
 	})
 	if err != nil {
 		fmt.Printf("Error %v\n", err)
@@ -36,4 +24,16 @@ func SendMessage(sess *session.Session, queueURL *string) {
 	}
 
 	fmt.Println("Message sent successfully !!")
+}
+
+func transformToSQSMap(msgMap map[string]string) map[string]*sqs.MessageAttributeValue {
+	// Create msg to the type accepted by SQS
+	msg := make(map[string]*sqs.MessageAttributeValue)
+	for k, v := range msgMap {
+		msg[k] = &sqs.MessageAttributeValue{
+			DataType:    aws.String("String"),
+			StringValue: aws.String(v),
+		}
+	}
+	return msg
 }
